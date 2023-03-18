60ccce238464d5514f5a3304ede04e5dc9961239
wuwawawa <1606441608@qq.com>
2023-03-18 10:56:01 +0800

    feat:图片上传实现

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/mapper/MediaFilesMapper.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/mapper/MediaFilesMapper.java
@@ -2,6 +2,7 @@ package com.xuecheng.media.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.media.model.po.MediaFiles;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -10,6 +11,7 @@ import com.xuecheng.media.model.po.MediaFiles;
  *
  * @author itcast
  */
+@Repository
 public interface MediaFilesMapper extends BaseMapper<MediaFiles> {
 
 }
