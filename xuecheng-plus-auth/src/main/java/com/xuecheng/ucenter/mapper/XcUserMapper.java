5f39fce20a1b5f2e3419cc19316816410d8a567b
wuwawawa <1606441608@qq.com>
2023-03-22 20:17:02 +0800

    feat:spring-security初步集成

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/mapper/XcUserMapper.java
@@ -0,0 +1,17 @@
+package com.xuecheng.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.ucenter.model.po.XcUser;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+@Repository
+public interface XcUserMapper extends BaseMapper<XcUser> {
+
+}
